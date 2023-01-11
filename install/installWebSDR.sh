21b950cdab2cb201e9c8db741eeed61b96152808
jaysik <33269670+jaysik@users.noreplay.github.com>
2023-01-10 20:47:12 -0700

    first commit

---
+++ install/installWebSDR.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# To install this application
+# Have the terminal in the same directory
+# Run the cmd "bash installWebSDR.sh" to execute the install script
+
+echo "Installing webSDR application";
+
+
+echo "Checking for installation of nvm...";
+if [ -d "${HOME}/.nvm/.git" ];
+    # nvm is installed
+    then 
+    
+    echo "nvm installed!";
+    echo "Checking if node 18.13.0 is installed..."
+    if [echo "${HOME}/.nvm"]
+
+    else echo "nvm not installed"; 
+    
+    fi
